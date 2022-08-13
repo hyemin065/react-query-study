6360c7a5d710f8c482646492f03a6621d4d0550d
hyemin065 <p62517@gmail.com>
2022-08-14 01:42:24 +0900

    react-query 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 function App() {
+  const func = async () => {
+    const { data } = await axios.get('https://api.coingecko.com/api/v3/exchanges?page=1');
+    return data;
+  };
+
+  const { data, isLoading, error, isError, isSuccess } = useQuery('exchange', func);
+  console.log(data, isSuccess);
+
+  // const info = useQuery('exchange', func);
+  // console.log(info);
+  if (isError) {
+    return <div>{(error as any).message}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type="button" onClick={func}>
+        sdfsdf
+      </button>
+      <div>
+        {isLoading ? <div>loading...</div> : ''}
+        {isSuccess &&
+          data.map((item: any) => {
+            return <div key={Math.random() * 100}>{item.name}</div>;
+          })}
+      </div>
+    </>
   );
 }
 
